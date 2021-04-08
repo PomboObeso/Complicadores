fcf4c28dd716894210eec0f2832c2325d6c4dd11
PomboObeso <matheus.gomes201@gmail.com>
2021-04-07 22:50:44 -0300

    Implementação da Main

--- src/program/Main.java
+++ src/program/Main.java
@@ -1,9 +1,26 @@
 package program;
 
+import exceptions.FTExceptions;
+import lexico.Lexicon;
+import lexico.Token;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Lexicon lex = new Lexicon(args[0]);
+		Token tk = null;
+		try {
+			do {
+				tk = lex.nextToken();
+				if(tk != null) {
+					System.out.println(tk);
+				}
+			}while(tk.lexema != "EOF");
+		}catch(FTExceptions ft) {
+			System.out.println("Erro Lexico:"+ft.getMessage());
+		}catch(Exception ex) {
+			System.out.println("Erro comum");
+		}
 	}
 
 }
