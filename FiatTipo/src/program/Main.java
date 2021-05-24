5fdb3155b4c9ba7a9d4bd960ad52053c7610ead4
PomboObeso <matheus.gomes201@gmail.com>
2021-05-24 11:44:19 -0300

    Adicionando ars[0]

--- FiatTipo/src/program/Main.java
+++ FiatTipo/src/program/Main.java
@@ -10,8 +10,8 @@ public class Main {
 		//Lexicon lex = new Lexicon("/home/beelzebub/eclipse-workspace/compilador_tipo/src/program/fibonacci.txt");
 		Token tk = null;
 		try {
-			String path = "/home/beelzebub/eclipse-workspace/compilador_tipo/src/program/shell.txt";
-			Sintatico sint = new Sintatico(path);
+			//String path = "/home/beelzebub/eclipse-workspace/compilador_tipo/src/program/shell.txt";
+			Sintatico sint = new Sintatico(args[0]);
 					
 		}catch(FTExceptions ft) {
 			System.out.println("Erro Lexico:"+ft.getMessage());
